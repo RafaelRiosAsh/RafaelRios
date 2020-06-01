237f632498f45bcbc7ce7f083d89ca725ced9d7b
RafaelRios <rafaeleduardoriosgarcia@gmail.com>
2020-06-01 00:26:28 -0500

    added sidebar that will hold the settings and other things in the future such as files

--- turing.js
+++ turing.js
@@ -7,6 +7,27 @@ editor.setOptions({
     fontSize: 13,
     enableBasicAutocompletion: true,
     enableSnippets: true,
-    enableLiveAutocompletion:   true
+    enableLiveAutocompletion:   true,
+    //printMargin: 140 to be used when we get the minimap view
+    printMargin: false
     
+});
+var toggle = true;
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        
+        if (toggle) {            
+            $('#Teditor').css("left","250px");
+            $('.moving-btn').css("left","250px");
+        }
+        else{
+            $('#Teditor').css("left","0");
+            $('.moving-btn').css("left","0");
+        }
+        toggle = !toggle;
+        
+    });
+
 });

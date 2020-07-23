8d35dc3c8cfe56232f4543fcd85f390f58c62605
RafaelRios <rafaeleduardoriosgarcia@gmail.com>
2020-07-22 20:04:40 -0500

    added a temperature and humidity ball in the center that imitates breathing

--- clock.js
+++ clock.js
@@ -1,20 +1,49 @@
 
+var api = "https://api.openweathermap.org/data/2.5/onecall?";
+var apikey = "87ad3cb8c6b0ab37445548f97e27137e";
+var units = "&units=metric";
+var weatherData;
+var redrawing = false;
 function setup() {
+  lat = 0;
+  lng = 0;
+
+  x =120;
+  y = 120;
+  
+  noises = 0.01;
+  
+  
+  N = 50;
+  time = 1;
+  grow = 1;
+
+  if (!navigator.geolocation) {
+    alert("weather data not available");
+  }
+  navigator.geolocation.getCurrentPosition(setPos);
+  // WEATHER ABOVE DELETE IF NECESSARY
+
   if(windowWidth<windowHeight){
     Width = windowWidth;
     Height = windowWidth;
+    divisor = 3.5;
   }
   else{
     Width = windowHeight;
     Height = windowHeight;
+    divisor = 5;
   }
   Year = Width - Width/10;
   Day = Year - Year/10;
   biggestCircle= Day - Year/5;
   mediumCircle = biggestCircle - Year /10;
   smallCircle = mediumCircle - Year/10;
+  weatherResizing = (smallCircle - Year/5)/2;
   strokeSize = 5*(Width/140);
   createCanvas(Width, Height);
+
+  r = weatherResizing;
   
   
   r1 = random(255); // r is a random number between 0 - 255
@@ -34,12 +63,38 @@ function setup() {
   gradient3 = lerpColor(color1, color2, .5);
   gradient4 = lerpColor(color1, color2, 0.8);
   gradient5 = lerpColor(color1, color2, 1);
+  gradient6 = color(r2+20,g2+20,b2+20);
    
 }
 
 function draw() {
   clear();
+
   
+
+  //this is the circle
+  time = time + 0.003;
+  noiseScale = (noises); 
+  noiseAmount = (20);
+  fill(gradient6);
+  beginShape();
+  for(i=0; i<N; i++){
+    x = width/2 + r*cos(TWO_PI*i/N);
+    y = height/2 + r*sin(TWO_PI*i/N);
+    x += map(noise(noiseScale*x,noiseScale*y,time),0,1,-noiseAmount,noiseAmount);
+    y += map(noise(noiseScale*x,noiseScale*y,time+1),0,1,-noiseAmount,noiseAmount);
+    vertex(x,y);
+  }
+  endShape();
+  
+  if (redrawing){
+    redrawing = false;
+    r = map(weatherData.current.temp,-40,40,5,weatherResizing, true);
+    noises = map(weatherData.current.humidity,30,100,0.01,0.08, true);
+    console.log("r: "+r+" noises: "+noises);
+  }
+  //cirlce ends here above
+
   /*strokeWeight(0); //for debugging
   fill(0)
   text(gradient1+' '+gradient2+' '+gradient3+' '+gradient4+' '+gradient5,20,20);
@@ -59,11 +114,16 @@ function draw() {
   arc(Width/2, Height/2, Day, Day, 0-HALF_PI, total(day(),31));
   stroke(gradient1);
   arc(Width/2, Height/2, Year, Year, 0-HALF_PI, total(month(),12));
+  
   drawCircles(60, smallCircle/2, gradient3,second());
   drawCircles(60, mediumCircle/2, gradient2, minute());
   drawCircles(24, biggestCircle/2, gradient1, hour());
   drawCircles(31, Day/2,gradient0, day());
   drawCircles(12, Year/2, gradient0, month());
+
+
+  //drawing circles for temperature
+  
 }
 
 
@@ -87,6 +147,29 @@ function drawCircles(shapeCount, circleSize, circleColor, num){
   for(i = num; i >= 0; i--){
     posx = circleSize * sin(i*shapeAngle) + width/2;
     posy = circleSize * -cos(i*shapeAngle) + height/2;
-    circle(posx, posy, strokeSize/4.5);
+    circle(posx, posy, strokeSize/divisor);
   }
+}
+
+function setPos(position) {
+  lat = position.coords.latitude;
+  lng = position.coords.longitude;
+  //console.log('lat: '+lat+' long: '+lng);
+  askWeather(lat, lng);
+}
+
+//used for weather gathering
+function gotData(data) {
+  redrawing = true;
+  weatherData = data;
+  //console.log(data);
+  //temp1 = Weather.current.weather.id;
+  temp = data.current.temp;
+  //id = data.current.weather.id;
+  //console.log("temp: "+temp+" id: "+data.current.humidity);
+}
+
+function askWeather(Lat, Long) {
+  var url = api+"lat="+Lat+"&lon="+Long+"&exclude=hourly,minutely,daily&appid="+apikey+units;
+  loadJSON(url, gotData);
 }
